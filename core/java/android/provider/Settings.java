3a3555f19cfd8d57c1a7dcc7d559143cb2e8339b
darkobas <darkobas@gmail.com>
2014-12-15 09:41:32 +0100

    Adblocker 2/3
    Change-Id: I44570d2eb4d791963dc0ba775eee40c80347a817

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2940,6 +2940,12 @@ public final class Settings {
          */
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
+         /**
+         * Disable ads (HFM)
+         * @hide
+         */
+        public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
