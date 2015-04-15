d2dc501cb14b92f590a2b56791ac2fb796576dc1
maxwen <max.weninger@gmail.com>
2015-04-15 03:30:38 +0200

    base: bring back expanding volume panel
    Change-Id: I9d077ef7c2d4cc8f388c7d42770420270113b576
    
    Conflicts:
    	core/res/res/values/custom_symbols.xml
    	packages/SystemUI/res/layout/volume_panel.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3156,6 +3156,13 @@ public final class Settings {
          public static final String VOLBTN_CURSOR_CONTROL = "volbtn_cursor_control";
 
         /**
+         * Boolean value whether to expand the volume panel
+         *
+         * @hide
+         */
+        public static final String VOLUME_PANEL_EXPANDED = "volume_link_expanded";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
