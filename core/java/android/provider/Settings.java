72a4c22f338d8fb7f28e5dfdd8ecd214d7e08df7
maxwen <max.weninger@gmail.com>
2015-05-07 00:36:54 +0200

    [1/2] base: custom softkey enabler
    allow enabling softkeys for hw button devices
    adds config to allow disabling the hw button
    actions + haptic feedback + backlight
    
    PS6:
    -use this to catch volume key event before sending when
    volume key wake is enabled
    
    Change-Id: I9bf039ddff793c89e4aa4d79d88bcc709e4146b6
    
    Conflicts:
    	core/java/com/android/internal/util/omni/DeviceUtils.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3524,6 +3524,12 @@ public final class Settings {
          public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+         * Show onscreen keys for hw button devices
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
