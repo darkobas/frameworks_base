d3b2b6363c6ec049ba73bad2e64f8b8aeeba0af7
maxwen <max.weninger@gmail.com>
2015-05-07 00:36:54 +0200

    [1/2] base: custom softkey enabler
    allow enabling softkeys for hw button devices
    adds config to allow disabling the hw button
    actions + haptic feedback + backlight
    
    PS6:
    -use this to catch volume key event before sending when
    volume key wake is enabled
    
    Change-Id: I9bf039ddff793c89e4aa4d79d88bcc709e4146b6
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3220,6 +3220,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Show onscreen keys for hw button devices
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
