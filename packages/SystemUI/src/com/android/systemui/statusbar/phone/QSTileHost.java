2e6a60688c6c2c7e73e1fa89efeefa833e0e7bc4
darkobas <darkobas@gmail.com>
2015-05-18 11:27:18 +0200

    Revert "QS: Nfc dynamic tile"
    This reverts commit be2d490cfc9117364e73f8ad7ba3b4d404c8fe2f.
    
    Conflicts:
    	packages/SystemUI/res/values/dud_strings.xml
    	packages/SystemUI/src/com/android/systemui/qs/tiles/NfcTile.java
    
    Change-Id: Ifc4aa73e28f75aa5027bed9468b8dd29c9d21277

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -39,7 +39,6 @@ import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
-import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.VisualizerTile;
@@ -265,7 +264,6 @@ public class QSTileHost implements QSTile.Host {
         else if (tileSpec.equals("cast")) return new CastTile(this);
         else if (tileSpec.equals("hotspot")) return new HotspotTile(this);
         else if (tileSpec.equals("usb_tether")) return new UsbTetherTile(this);
-        else if (tileSpec.equals("nfc")) return new NfcTile(this);
         else if (tileSpec.equals("visualizer")) return new VisualizerTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
