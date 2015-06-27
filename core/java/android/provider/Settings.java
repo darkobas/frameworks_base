778003afbf0557d56f77d9033d9ee9ee9d7a07e1
lion0738 <lion0738@naver.com>
2015-06-27 16:38:52 -0300

    [1/2] Base: option to launch heads up in floating mode.
    Port By BlackDragon.
    Used reference: scotthartbti/android_frameworks_base@06dd21f
    
    Added Modified NotificationHelper for LP.
    
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>
    
    Change-Id: I312501371e9066ea7bed12a8027c73a4396b90cb
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3300,6 +3300,12 @@ public final class Settings {
          */
         public static final String SCREEN_BRIGHTNESS_DOZE = "screen_brightness_doze";
 
+        /**
+         * Launch headsup in floating window or not
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_FLOATING = "heads_up_floating";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
