3237eb0b8f510744400ff41d57e93fd5394b63ef
Diogo Ferreira <diogo@underdev.org>
2015-11-23 15:35:36 +0000

    services: devicepolicy: Restore backup service as system
    Restoring the backup service must be performed by either system
    or root otherwise it will cause a SecurityException. This patch
    clears the uid, performs the call as system and then restores it.
    
    Fixes the following CTS tests:
    
    com.android.cts.devicepolicy.CustomDeviceOwnerTest
      -- testOwnerChangedBroadcast
      -- testSilentPackageInstall
    
    com.android.cts.devicepolicy.DeviceOwnerTest
      -- testCaCertManagement
      -- testCannotSetDeviceOwnerAgain
      -- testDeviceOwnerSetup
      -- testKeyManagement
      -- testLockTask
      -- testSystemUpdatePolicy
      -- testWifiConfigLockdown
    
    com.android.cts.devicepolicy.MixedDeviceOwnerTest
      -- testAccountManagement
      -- testApplicationRestrictions
      -- testDelegatedCertInstaller
      -- testPermissionGrant
      -- testPermissionGrantPreMApp
      -- testPermissionMixedPolicies
      -- testPermissionPolicy
      -- testPermissionPrompts
      -- testPersistentIntentResolving
      -- testScreenCaptureDisabled
    
    Change-Id: Ia26222f1fece14217554459c42679f82f7ceb980

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -4236,6 +4236,17 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
                 mDeviceOwner.clearDeviceOwner();
                 mDeviceOwner.writeOwnerFile();
                 updateDeviceOwnerLocked();
+                // Restore backup manager.
+                long ident = Binder.clearCallingIdentity();
+                try {
+                    IBackupManager ibm = IBackupManager.Stub.asInterface(
+                            ServiceManager.getService(Context.BACKUP_SERVICE));
+                    ibm.setBackupServiceActive(UserHandle.USER_OWNER, true);
+                } catch (RemoteException e) {
+                    throw new IllegalStateException("Failed activating backup service.", e);
+                } finally {
+                    Binder.restoreCallingIdentity(ident);
+                }
             }
         }
     }
