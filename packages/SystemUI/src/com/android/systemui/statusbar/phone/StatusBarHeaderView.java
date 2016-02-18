1940e0444b451b86baa646f6ecf28a443b3f5eaa
maxwen <max.weninger@gmail.com>
2016-02-19 00:14:15 +0100

    [1/2] base: add config to apply custom shadow on header images
    To make text and status icons better visible with bright
    images if required.
    
    Change-Id: I8a0c601316a24f3c89bcdee20c2885d248d5b4b5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -27,6 +27,7 @@ import android.content.res.Resources;
 import android.graphics.Outline;
 import android.graphics.Rect;
 import android.graphics.drawable.Animatable;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.RippleDrawable;
 import android.net.Uri;
@@ -929,6 +930,19 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         } else {
             mBackgroundImage.setImageDrawable(dw);
         }
+        applyHeaderBackgroundShadow();
+    }
+
+    private void applyHeaderBackgroundShadow() {
+        final int headerShadow = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW, 0,
+                UserHandle.USER_CURRENT);
+
+        if (headerShadow != 0 && mBackgroundImage != null) {
+            ColorDrawable shadow = new ColorDrawable(Color.BLACK);
+            shadow.setAlpha(headerShadow);
+            mBackgroundImage.setForeground(shadow);
+        }
     }
 
     @Override
@@ -941,6 +955,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                 doUpdateStatusBarCustomHeader(headerImage, force);
             }
         });
+        applyHeaderBackgroundShadow();
     }
 
     @Override
