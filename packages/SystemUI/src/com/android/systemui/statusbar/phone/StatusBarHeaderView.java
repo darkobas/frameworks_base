3acc6347399065e74aaa0288cddca513330da3d1
Marko Man <darkobas@gmail.com>
2016-02-25 10:57:40 +0100

    Revert "[1/2] base: add config to apply custom shadow on header images"
    This reverts commit 1940e0444b451b86baa646f6ecf28a443b3f5eaa.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -27,7 +27,6 @@ import android.content.res.Resources;
 import android.graphics.Outline;
 import android.graphics.Rect;
 import android.graphics.drawable.Animatable;
-import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.RippleDrawable;
 import android.net.Uri;
@@ -930,19 +929,6 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         } else {
             mBackgroundImage.setImageDrawable(dw);
         }
-        applyHeaderBackgroundShadow();
-    }
-
-    private void applyHeaderBackgroundShadow() {
-        final int headerShadow = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW, 0,
-                UserHandle.USER_CURRENT);
-
-        if (headerShadow != 0 && mBackgroundImage != null) {
-            ColorDrawable shadow = new ColorDrawable(Color.BLACK);
-            shadow.setAlpha(headerShadow);
-            mBackgroundImage.setForeground(shadow);
-        }
     }
 
     @Override
@@ -955,7 +941,6 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                 doUpdateStatusBarCustomHeader(headerImage, force);
             }
         });
-        applyHeaderBackgroundShadow();
     }
 
     @Override
