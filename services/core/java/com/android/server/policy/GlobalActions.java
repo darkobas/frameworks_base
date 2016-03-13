385a13d3d652d43e1273b50802dc6715f863e08e
DvTonder <david.vantonder@gmail.com>
2016-03-13 22:46:58 +0100

    fix screenshot in powermenu delay

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -1655,14 +1655,16 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                             };
                             msg.replyTo = new Messenger(h);
                             msg.arg1 = msg.arg2 = 0;
-                            if (mStatusBar != null && mStatusBar.isVisibleLw())
-                                msg.arg1 = 1;
-                            if (mNavigationBar != null && mNavigationBar.isVisibleLw())
-                                msg.arg2 = 1;
-                            try { messenger.send(msg); }
-                            catch (RemoteException e) {}
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException ie) {
+                            }
+                            try {
+                                messenger.send(msg);
+                            } catch (RemoteException e) {
                         }
                     }
+                }
                     @Override
                     public void onServiceDisconnected(ComponentName name) {}
                 };
