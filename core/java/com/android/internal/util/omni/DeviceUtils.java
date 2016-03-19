0c6a70b983d566e2dcc8d46325e46e36d910039f
maxwen <max.weninger@gmail.com>
2016-03-19 17:24:36 +0100

    base: add check API for flashlight to DeviceUtils
    so clients can check if flashlight is available
    
    Change-Id: I092c1592021a5d615a5dd73f2f4a10c5b58e621e

--- core/java/com/android/internal/util/omni/DeviceUtils.java
+++ core/java/com/android/internal/util/omni/DeviceUtils.java
@@ -20,6 +20,9 @@ import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
 import android.hardware.SensorManager;
 import android.net.ConnectivityManager;
 import android.nfc.NfcAdapter;
@@ -162,4 +165,21 @@ public class DeviceUtils {
         return getScreenType(con) == DEVICE_TABLET;
     }
 
+    public static boolean deviceSupportsCameraFlashlight(Context context) {
+        try {
+            CameraManager cameraManager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
+            String[] ids = cameraManager.getCameraIdList();
+            for (String id : ids) {
+                CameraCharacteristics c = cameraManager.getCameraCharacteristics(id);
+                Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
+                if (flashAvailable != null && flashAvailable
+                        && lensFacing != null && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                    return true;
+                }
+            }
+        } catch (CameraAccessException e) {
+        }
+        return false;
+    }
 }
