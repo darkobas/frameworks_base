fceed7902114c939541d6f435815939f26a47570
Danesh M <danesh@cyngn.com>
2016-05-06 15:35:28 -0700

    WindowManagerService : Ensure we update both size/position
    computeShownBounds is necessary to ensure shownFrame is up to
    date when setSurfaceBoundariesLocked is invoked to ensure any needed
    size and positional changes go as part of one transaction. Relying on
    animateLocked to send changes results in size/position being sent as two
    separate transactions and surface flinger drawing a frame which looks incorrect
    according to what the window specified.
    
    Change-Id: If881047c060c73db0037fda7da1f66a89dd45487

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -10132,6 +10132,7 @@ public class WindowManagerService extends IWindowManager.Stub
                             }
                         }
 
+                        winAnimator.computeShownFrameLocked();
                         winAnimator.setSurfaceBoundariesLocked(recoveringMemory);
                     }
 
